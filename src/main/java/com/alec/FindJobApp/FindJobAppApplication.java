dd31841fe0528e9b684224d2b2eb415371df665e
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2025-11-09 15:41:16 +0200

    finished project

---
+++ src/main/java/com/alec/FindJobApp/FindJobAppApplication.java
@@ -0,0 +1,14 @@
+package com.alec.FindJobApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindJobAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FindJobAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alec/FindJobApp/FindJobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alec.FindJobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindJobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
